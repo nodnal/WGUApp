de79084d63754ab93a0fdaacb6623fa8c2e0a486
nodnal <landon.s.adrian@gmail.com>
2019-10-30 17:36:26 -0400

    Added Assessment list item layout and the adapter for it. Added viewmodel and recyclerview references to CourseAssessmentFragment.

---
+++ app/src/main/java/com/example/wguapp/AssessmentListAdapter.java
@@ -0,0 +1,82 @@
+package com.example.wguapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.wguapp.db.entity.Assessment;
+import com.example.wguapp.db.entity.Course;
+import com.example.wguapp.ui.OnViewHolderBindCallback;
+import com.example.wguapp.util.DateUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AssessmentListAdapter extends RecyclerView.Adapter<AssessmentListAdapter.AssessmentViewHolder> {
+
+    private List<Assessment> assessments;
+
+
+    public OnViewHolderBindCallback OnViewHolderBind;
+
+    public AssessmentListAdapter(OnViewHolderBindCallback cb){
+        this.OnViewHolderBind = cb;
+    }
+
+    @NonNull
+    @Override
+    public AssessmentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.term_list_item_layout, parent, false);
+        AssessmentViewHolder holder = new AssessmentViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AssessmentViewHolder holder, int position) {
+        Assessment assessment = assessments.get(position);
+        holder.title.setText(assessment.Title);
+        holder.type.setText(assessment.Type);
+        holder.goalDate.setText(DateUtil.toString(assessment.GoalDate));
+        if (OnViewHolderBind != null){
+            OnViewHolderBind.onViewHolderBind(holder, position);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (assessments != null)
+            return assessments.size();
+        else return 0;
+    }
+
+
+    public class AssessmentViewHolder extends RecyclerView.ViewHolder{
+        public TextView title;
+        public TextView type;
+        public TextView goalDate;
+
+
+        public AssessmentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.assessment_list_item_title);
+            type = itemView.findViewById(R.id.assessment_list_item_type);
+            type = itemView.findViewById(R.id.assessment_list_item_type);
+
+
+        }
+
+    }
+
+    public void setAssessments(List<Assessment> assessments){
+        this.assessments= assessments;
+        notifyDataSetChanged();
+    }
+
+}
--- app/src/main/java/com/example/wguapp/ui/CourseAssessmentFragment.java
+++ app/src/main/java/com/example/wguapp/ui/CourseAssessmentFragment.java
@@ -1,21 +1,36 @@
 package com.example.wguapp.ui;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.wguapp.AssessmentListAdapter;
 import com.example.wguapp.R;
+import com.example.wguapp.db.entity.Assessment;
+import com.example.wguapp.viewmodel.CourseDetailViewModel;
+
+import java.security.cert.Extension;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CourseAssessmentFragment extends Fragment {
+public class CourseAssessmentFragment extends Fragment implements OnViewHolderBindCallback{
 
+    private RecyclerView listView;
+    private AssessmentListAdapter adapter;
+    private LiveData<List<Assessment>> assessments;
+    private CourseDetailViewModel vm;
 
     public CourseAssessmentFragment() {
         // Required empty public constructor
@@ -30,7 +45,31 @@ public class CourseAssessmentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_course_assessment, container, false);
+        View view = inflater.inflate(R.layout.fragment_course_assessment, container, false);
+        listView = view.findViewById(R.id.course_detail_assesment_list);
+        adapter = new AssessmentListAdapter(this);
+        listView.setAdapter(adapter);
+        listView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initViewModel();
+    }
+
+    private void initViewModel() {
+        vm = ViewModelProviders.of(getActivity()).get(CourseDetailViewModel.class);
+        assessments = vm.getAssessments();
+    }
+
+    @Override
+    public void onViewHolderBind(RecyclerView.ViewHolder viewHolder, int position) {
+        viewHolder.itemView.setOnClickListener((view) -> {
+            Intent intent = new Intent(this.getContext(), CourseDetailActivity.class);
+            intent.putExtra("course_id", assessments.getValue().get(viewHolder.getAdapterPosition()).getId());
+            startActivity(intent);
+        });
+    }
 }
